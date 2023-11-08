7214e5b4746a0561f37d11d0db4157c3a71fd36a
Eugen Soloviov <suenot@gmail.com>
2023-11-09 01:06:32 +0300

    fix empty wallets

--- src/db/deep/emission-insert-handler.ts
+++ src/db/deep/emission-insert-handler.ts
@@ -2,7 +2,7 @@ async ({data, deep}) => {
   const itemValue = data?.newLink?.value?.value;
   const fromId = data?.newLink?.from_id;
   const toId = data?.newLink?.to_id;
-  const toValue = (await deep.select(toId)).data?.[0]?.value?.value;
+  const toValue = (await deep.select(toId)).data?.[0]?.value?.value || 0;
   const toValueResult = toValue + itemValue;
   const updateToResult = await deep.update(
     {
--- src/db/deep/transaction-insert-handler.ts
+++ src/db/deep/transaction-insert-handler.ts
@@ -2,8 +2,8 @@ async ({data, deep}) => {
   const itemValue = data?.newLink?.value?.value;
   const fromId = data?.newLink?.from_id;
   const toId = data?.newLink?.to_id;
-  const fromValue = (await deep.select(fromId)).data?.[0]?.value?.value;
-  const toValue = (await deep.select(toId)).data?.[0]?.value?.value;
+  const fromValue = (await deep.select(fromId)).data?.[0]?.value?.value || 0;
+  const toValue = (await deep.select(toId)).data?.[0]?.value?.value || 0;
   const fromValueResult = fromValue - itemValue;
   const toValueResult = toValue + itemValue;
   const updateFromResult = await deep.update(
@@ -28,5 +28,6 @@ async ({data, deep}) => {
       table: 'numbers'
     }
   )
+
   return {itemValue, fromValue, toValue, fromValueResult, toValueResult, updateFromResult, updateToResult}; 
 }
