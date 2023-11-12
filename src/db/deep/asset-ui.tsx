cba58a8531eaf4d085c3e50afaf84aac5ea56389
Eugen Soloviov <suenot@gmail.com>
2023-11-12 23:52:40 +0300

    fix

--- src/db/deep/asset-ui.tsx
+++ src/db/deep/asset-ui.tsx
@@ -1,11 +1,9 @@
 async ({ deep, require }) => {
   const React = require('react');
   const { useState, useEffect } = React;
-  const { Box, Text, Avatar, Wrap, WrapItem, Editable, EditablePreview, EditableInput, EditableTextarea, Center, Flex, Divider } = require('@chakra-ui/react');
+  const { Box, Text, Avatar, Wrap, WrapItem, Editable, EditablePreview, EditableInput, EditableTextarea, Center, Flex, Divider, Button } = require('@chakra-ui/react');
   const AsyncFileId = await deep.idLocal("@deep-foundation/core", "AsyncFile");
-
-  // const ContainId = await deep.idLocal("@deep-foundation/core", "Contain");
-  var AssetNameId = deep.id("@suenot/asset", "Name");
+  var AssetNameId = await deep.id("@suenot/asset", "Name");
   var AssetDescriptionId = await deep.id("@suenot/asset", "Description");
   var AssetTickerId = await deep.id("@suenot/asset", "Ticker");
   var AssetAvatarId = await deep.id("@suenot/asset", "Avatar");
@@ -60,21 +58,19 @@ async ({ deep, require }) => {
 
 
 
-    const assetFileValue = fileData?.[0]?.id && `/api/file?linkId=${fileData?.[0]?.id}`;
-    const assetAvatarValue = avatarData?.[0]?.value?.value;
-    const assetAvatarId = avatarData?.[0]?.id;
+    var assetFileValue = fileData?.[0]?.id && `/api/file?linkId=${fileData?.[0]?.id}`;
+    var assetAvatarValue = avatarData?.[0]?.value?.value;
+    var assetAvatarId = avatarData?.[0]?.id;
     // File has more priority than avatar (url)
-    const assetSrcValue = assetFileValue || assetAvatarValue || "";
+    var assetSrcValue = assetFileValue || assetAvatarValue || "";
 
-    const assetNameValue = nameData?.[0]?.value?.value || '';
-    const assetNameId = nameData?.[0]?.id;
-    const assetTickerValue = tickerData?.[0]?.value?.value || '';
-    const assetTickerId = tickerData?.[0]?.id;
-    const assetDescriptionValue = assetDescriptionData?.[0]?.value?.value || "";
-    const assetDescriptionId = assetDescriptionData?.[0]?.id;
+    var assetNameValue = nameData?.[0]?.value?.value || '';
+    var assetNameId = nameData?.[0]?.id;
+    var assetTickerValue = tickerData?.[0]?.value?.value || '';
+    var assetTickerId = tickerData?.[0]?.id;
+    var assetDescriptionValue = assetDescriptionData?.[0]?.value?.value || "";
+    var assetDescriptionId = assetDescriptionData?.[0]?.id;
     console.log({data});
-
-
 
     const [assetName, setAssetName] = useState(assetNameValue);
     const [assetTicker, setAssetTicker] = useState(assetTickerValue);
@@ -82,102 +78,6 @@ async ({ deep, require }) => {
     const [assetFile, setAssetFile] = useState(assetFileValue);
     const [assetAvatar, setAssetAvatar] = useState(assetAvatarValue);
     const [assetSrc, setAssetSrc] = useState(assetSrcValue);
-
-
-    useEffect( () => {
-      console.log('useEffect: assetName');
-      console.log({assetNameId, assetName});
-      const insertUpdateAssetName = async () => {
-        if (!assetNameId) {
-          console.log('!assetNameId')
-          const { data: [{ id: _assetNameId }] } = await deep.insert({
-            type_id: AssetNameId,
-            from_id: link.id,
-            to_id: link.id,
-            string: { data: { value: assetName } },
-          })
-          assetNameId = _assetNameId;
-        } else {
-          console.log('else');
-          const { data: [{ link: _assetNameId }] } = await deep.update(
-            { link_id: assetNameId },
-            { value: assetName },
-            { table: 'strings', returning: `link { ${deep.selectReturning} }` }
-          );
-        }
-      }
-      insertUpdateAssetName();
-    }, [assetName]);
-
-    useEffect( () => {
-      console.log('useEffect: assetDescription');
-      console.log({assetDescriptionId, assetDescription});
-      const insertUpdateAssetDescription = async () => {
-        if (!assetDescriptionId) {
-          console.log("Description doesn't exist")
-          const { data: [{ id: _assetDescriptionId }] } = await deep.insert({
-            type_id: AssetDescriptionId,
-            from_id: link.id,
-            to_id: link.id,
-            string: { data: { value: assetDescription } },
-          })
-          assetDescriptionId = _assetDescriptionId;
-          deep.minilinks.apply([_assetDescriptionId]);
-        } else {
-          console.log("Description exist")
-          const { data: [{ link: _assetDescriptionId }] } = await deep.update(
-            { link_id: assetDescriptionId },
-            { value: assetDescription },
-            { table: 'strings', returning: `link { ${deep.selectReturning} }` }
-          );
-          console.log({_assetDescriptionId});
-        }
-      }
-      insertUpdateAssetDescription();
-    }, [assetDescription]);
-
-    useEffect( () => {
-      const insertUpdateAssetTicker = async () => {
-        if (!assetTickerId) {
-          const { data: [{ id: _assetTickerId }] } = await deep.insert({
-            type_id: AssetTickerId,
-            from_id: link.id,
-            to_id: link.id,
-            string: { data: { value: assetTicker } },
-          })
-          assetTickerId = _assetTickerId;
-        } else {
-          const { data: [{ link: _assetTickerId }] } = await deep.update(
-            { link_id: assetTickerId },
-            { value: assetTicker },
-            { table: 'strings', returning: `link { ${deep.selectReturning} }` }
-          );
-        }
-      }
-      insertUpdateAssetTicker();
-    }, [assetTicker]);
-
-    useEffect( () => {
-      const insertUpdateAssetAvatar = async () => {
-        if (!assetAvatarId) {
-          const { data: [{ id: _assetAvatarId }] } = await deep.insert({
-            type_id: AssetAvatarId,
-            from_id: link.id,
-            to_id: link.id,
-            string: { data: { value: assetAvatar } },
-          })
-          assetAvatarId = _assetAvatarId;
-        } else {
-          const { data: [{ link: _assetAvatarId }] } = await deep.update(
-            { link_id: assetAvatarId },
-            { value: assetAvatar },
-            { table: 'strings', returning: `link { ${deep.selectReturning} }` }
-          );
-        }
-      }
-      insertUpdateAssetAvatar();
-    }, [assetAvatar]);
-
 
     return <div>
       <Box maxW='sm' minW='sm' w='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' p='4' backgroundColor='white'>
@@ -216,6 +116,84 @@ async ({ deep, require }) => {
           <EditablePreview w={'100%'} />
           <EditableInput />
         </Editable>
+        <Button colorScheme='teal' size='md' variant='outline' onClick={async () => {
+          if (assetName === undefined) setAssetName("");
+          if (assetDescription === undefined) setAssetDescription("");
+          if (assetTicker === undefined) setAssetTicker("");
+          if (assetAvatar === undefined) setAssetAvatar("");
+
+          if (!assetNameId) {
+            console.log("Asset doesn't exist")
+            const { data: [{ id: _assetNameId }] } = await deep.insert({
+              type_id: AssetNameId,
+              from_id: link.id,
+              to_id: link.id,
+              string: { data: { value: assetName } },
+            })
+            assetNameId = _assetNameId;
+          } else {
+            console.log("Asset exist", {assetNameId, AssetNameId, assetName})
+            const { data: [{ link: _assetNameId }] } = await deep.update(
+              { link_id: assetNameId },
+              { value: assetName },
+              { table: 'strings', returning: `link { ${deep.selectReturning} }` }
+            );
+            console.log({_assetNameId});
+          }
+
+          if (!assetDescriptionId) {
+            console.log("Description doesn't exist")
+            const { data: [{ id: _assetDescriptionId }] } = await deep.insert({
+              type_id: AssetDescriptionId,
+              from_id: link.id,
+              to_id: link.id,
+              string: { data: { value: assetDescription } },
+            })
+            assetDescriptionId = _assetDescriptionId;
+          } else {
+            console.log("Description exist")
+            const { data: [{ link: _assetDescriptionId }] } = await deep.update(
+              { link_id: assetDescriptionId },
+              { value: assetDescription },
+              { table: 'strings', returning: `link { ${deep.selectReturning} }` }
+            );
+            console.log({_assetDescriptionId});
+          }
+
+          if (!assetTickerId) {
+            const { data: [{ id: _assetTickerId }] } = await deep.insert({
+              type_id: AssetTickerId,
+              from_id: link.id,
+              to_id: link.id,
+              string: { data: { value: assetTicker } },
+            })
+            assetTickerId = _assetTickerId;
+          } else {
+            const { data: [{ link: _assetTickerId }] } = await deep.update(
+              { link_id: assetTickerId },
+              { value: assetTicker },
+              { table: 'strings', returning: `link { ${deep.selectReturning} }` }
+            );
+          }
+
+          if (!assetAvatarId) {
+            const { data: [{ id: _assetAvatarId }] } = await deep.insert({
+              type_id: AssetAvatarId,
+              from_id: link.id,
+              to_id: link.id,
+              string: { data: { value: assetAvatar } },
+            })
+            assetAvatarId = _assetAvatarId;
+          } else {
+            const { data: [{ link: _assetAvatarId }] } = await deep.update(
+              { link_id: assetAvatarId },
+              { value: assetAvatar },
+              { table: 'strings', returning: `link { ${deep.selectReturning} }` }
+            );
+          }
+        }}>
+          Save
+        </Button>
       </Box>
     </div>;
   }
