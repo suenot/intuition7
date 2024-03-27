ed45b46b62fcd5e7c711f86e34e302e2847972d3
Eugen Soloviov <suenot@gmail.com>
2024-03-27 15:05:07 +0300

    fix

--- src/signals/newListing/upsertListingInstrument.ts
+++ src/signals/newListing/upsertListingInstrument.ts
@@ -1,7 +1,7 @@
 import { store } from './store';
 import { Instrument } from '../../types';
 
-export const upsertListingInstrument = (listingInstruments: Instrument[]) => {]
+export const upsertListingInstrument = (listingInstruments: Instrument[]) => {
   for (const instrument of listingInstruments) {
     store.listingInstruments[instrument.id] = instrument;
   }
